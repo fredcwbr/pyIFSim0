92082539923e16a2950fa468686d8e1e0901b238
fredcw <fredcw@nb01>
2024-10-27 18:27:36 -0300

    Inclusao do elevador + refatoracao do botao

--- simula/simula.py
+++ simula/simula.py
@@ -1,13 +1,35 @@
 
 from enum import Enum
+import random
 
 import nomesX as nomes
 
 
 
-class BOTAO(Enum):
+class eBOTAO(Enum):
     DESLIGADO = 0,
     LIGADO = 1
+
+class BOTAO:
+    DESLIGADO = eBOTAO.DESLIGADO
+    LIGADO = eBOTAO.LIGADO
+
+
+    def __init__(self):
+        self.__estado__ = type(self).DESLIGADO
+        
+    def ativa(self):
+        self.__estado__ = type(self).LIGADO
+        
+    def desativa(self):
+        self.__estado__ = type(self).DESLIGADO
+
+    @property 
+    def estado(self):
+        return self.__estado__
+
+    def  __str__(self):
+        return "estado: {} ".format( self.__estado__ )
 
 
 class cPosicao:
@@ -85,18 +107,35 @@ class cPessoa( cIdentidade ):
 
 
 
+class cElevador:
+    
+    def __init__( self, numero, andaresPredio ):
+        # define um andar do predio
+        # andar pode conter pessoas
+        # e tem os botoes de sinalizacao dos elevadores
+        #
+        self.emTransito = False
+        self.qualAndar = 0
+        self.numeroDoElevador = numero
+        #cPessoa
+        self.noAndarParaSair = []
+        #cPessoa
+        self.noPainelIndicador = list( BOTAO() for B in range(andaresPredio ) )  
+        
+    def  __str__(self):
+        return "cElevador :: {} >> {}".format( self.numeroDoElevador,
+                                    [ B.estado for B in self.noPainelIndicador] ) 
 
 
 class cAndar:
-
     
     def __init__( self, numero ):
         # define um andar do predio
         # andar pode conter pessoas
         # e tem os botoes de sinalizacao dos elevadores
         #
-        self.subir = cBotao(BOTAO.DESLIGADO)
-        self.descer = cBotao(BOTAO.DESLIGADO)
+        self.subir = BOTAO()
+        self.descer = BOTAO()
 
         #cPessoa
         self.noAndarParaSair = []
@@ -107,15 +146,27 @@ class cAndar:
 
 class cPredio( cDestino ):
  
-    def __init__( self, andares ):
+    def __init__( self, destino, andares ):
+        super().__init__( destino )
         # inicializa o predio com seus andares,
         #
-        self.niveis = cAndares( andares )
+        self.niveis = cAndar( andares )
+        self.elevadores =  list( cElevador( N , andares ) for N in range(5) )
+
+
+        print("Elevadores >> \n")
+        for C in self.elevadores:
+            print( "{} , {}\n".format( C,  str(C) ) )
         
 
 
 if __name__ == "__main__":
     pessoasX = []
+    predios = []
+
+    
+    for px in range(1,10):
+        predios.append( cPredio( cDestino("Predio {}".format(px) ) , int(random.random() * 10) ) )
 
     for P in nomes.nomesPessoas():
         s =  cPessoa(P, cDestino("casa") , cDestino("casa") )
