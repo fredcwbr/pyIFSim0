89703254d552921c6b4ffa9acd1e0240323d246b
fredcw <fredcw@teste0itx>
2024-10-31 20:43:52 -0300

    Incluido elementos para nomes de bairros, Facilidades para geracao de nomes incluido codigo inicial com suporte de locks no andar

--- simula/nomesX.py
+++ simula/nomesX.py
@@ -1,9 +1,76 @@
 
 
+import random
+
+class Nomes:
+    PN = []
+    SN = []
+    CDs = []
+    BRs = []
+
+    def __init__(self):
+        # Carrega os nomes
+        with open("nomesPessoa.txt", "r") as xnome:
+            self.__class__.PN = xnome.readlines()
+        # Carrega os sobrenomes
+        with open("sobreNomesPessoa.txt", "r") as xnome:
+            self.__class__.SN = xnome.readlines()
+        # Compoe nomes e sobrenomes aleatoriamente
+        #
+        with open("cidades.csv", "r") as xnome:
+            self.__class__.CDs = xnome.readlines()
+        #
+        with open("bairros.csv", "r") as xnome:
+            self.__class__.BRs = xnome.readlines()
+
+    @classmethod
+    def Pessoa(self):
+        while(1):        
+            yield self.PN[random.randrange(0,len(self.PN))].strip()+" "+self.SN[random.randrange(0,len(self.SN))].strip()
+
+    @classmethod
+    def prenome(self):
+        while(1):        
+            yield self.PN[random.randrange(0,len(self.PN))].strip()
+            
+    @classmethod
+    def sobrenome(self):
+        while(1):
+            yield self.SN[random.randrange(0,len(self.SN))].strip()
+
+    @classmethod
+    def Cidades(self):
+        while(1):
+            cd = random.randrange(0,len(self.CDs))
+            yield (cd, self.CDs[cd].strip() )
+
+    @classmethod
+    def getCdCidades(self):
+        while(1):
+            yield random.randrange(0,len(self.CDs))
+
+    @property
+    def nomeCidade(self,cdCidade):
+        return self.__class__.CDs[cdCidade]
 
 
-def nomesPessoas():
-    with open("nomesPessoa.txt", "r") as xnome:
-        for N in xnome.readlines():
-            yield N.strip()
+    @classmethod
+    def Bairro(self):
+        while(1):
+            cd = random.randrange(0,len(self.BRs))
+            yield (cd, self.BRs[cd].strip() )
+
+    @classmethod
+    def getCdBairro(self):
+        while(1):
+            yield random.randrange(0,len(self.BRs))
+
+    @property
+    def nomeBairro(self,cdBR):
+        return self.__class__.BRs[cdBR]
+
+
+
+
+
 
--- simula/simula.py
+++ simula/simula.py
@@ -1,14 +1,40 @@
 
-from enum import Enum
+
+import concurrent.futures
+import logging
+import queue
 import random
+import threading
+import time
 
-import nomesX as nomes
+from enum import Enum
 
+from  nomesX import Nomes
+
+Cadastros = Nomes()
+
+
+class eDIRECAO(Enum):
+    PARADO = 0,
+    SUBIR = 1,
+    DESCER = 2
+
+    __strsX__ = ["PARADO", "SUBIR", "DESCER" ]
+    
+    def  __str__(self):
+        return __strsX__[self] 
 
 
 class eBOTAO(Enum):
     DESLIGADO = 0,
     LIGADO = 1
+
+    def  __str__(self):
+        if self == eBOTAO.LIGADO:
+            return "LIGADO"
+        else:
+            return "DESLIGADO" 
+    
 
 class BOTAO:
     DESLIGADO = eBOTAO.DESLIGADO
@@ -29,7 +55,7 @@ class BOTAO:
         return self.__estado__
 
     def  __str__(self):
-        return "estado: {} ".format( self.__estado__ )
+        return "estado: {} ".format( str(self.__estado__) )
 
 
 class cPosicao:
@@ -89,7 +115,7 @@ class cDestino( cIdentidade ) :
         self.posicaoGeo   = 0
 
     def  __str__(self):
-        return " cDestino :: {}".format( self.nome, super().__str__()  )
+        return "{} :: {}\n".format( self.nome, super().__str__()  )
 
 
 class cPessoa( cIdentidade ):
@@ -100,13 +126,19 @@ class cPessoa( cIdentidade ):
         self.predioCasa = dfltCasa
         self.predioDestino = dfltDest
         self.emTransito = False
-        # print("Iniciando pessoa {}".format(nome))
+        logging.debug("Iniciando pessoa {}".format(nome))
 
     def  __str__(self):
-        return " cPessoa :: {} >> {}".format( self.nome,super().__str__() )
+        return " cPessoa :: {} >> {}\n".format( self.nome,super().__str__() )
 
-
-
+class cEstadoElevador(Enum): 
+        PARADO = 0,
+        SUBINDO = 1,
+        DESCENDO = 2,
+        MANUTENCAO = 3,
+        CONGELADO = 4,
+        TESTE = 5
+    
 class cElevador:
     
     def __init__( self, numero, andaresPredio ):
@@ -121,10 +153,13 @@ class cElevador:
         self.noAndarParaSair = []
         #cPessoa
         self.noPainelIndicador = list( BOTAO() for B in range(andaresPredio ) )  
+
+    def action(self):
+        #
+        pass
         
     def  __str__(self):
-        return "cElevador :: {} >> {}".format( self.numeroDoElevador,
-                                    [ B.estado for B in self.noPainelIndicador] ) 
+        return  "cElevador :: "+ str(self.numeroDoElevador) + "{}\n".format( [ str(B.estado) for B in self.noPainelIndicador ] ) 
 
 
 class cAndar:
@@ -134,45 +169,126 @@ class cAndar:
         # andar pode conter pessoas
         # e tem os botoes de sinalizacao dos elevadores
         #
+        self._lock = threading.Lock()
         self.subir = BOTAO()
         self.descer = BOTAO()
 
         #cPessoa
-        self.noAndarParaSair = []
+        self.noAndarParaSair = {}
         #cPessoa
-        self.noAndarOcupado = []
+        self.noAndarOcupado =  {}
 
+    def chegouElevador(self, elev):
+        # O elevador chegou,
+        #
+        encheu = False
+        for K in self.noAndarParaSair.keys():
+            # veja se essa pessoa esta indo na
+            # mesma direcao do elevador.,
+            #
+            if self.noAndarParaSair[K].SobeOuDesce != elev.direcao:
+                # nao é pra mim,
+                #
+                continue
+            else:
+                # mesma direcao ., ..
+                # tenta entrar se houver espaco
+                self._lock.acquire()
+                try:
+                    elev.entra(self.noAndarParaSair[K])
+                    # entrou no elevador, .. contabiliza no andar
+                    self.noAndarParaSair.pop(K)
+                except ElevadorCheio:
+                    # Nao tem mais espaco no Elevador
+                    logging.debug(str(elev))
+                    encheu = True
+                except ElevadorEmManutencao:
+                    # Nao tem como entrar, esta em manutencao
+                    logging.debug(str(elev))
+                finally:
+                    self._lock.release()
+                # Nao precisamos mais ficar aqui..
+                # .. Ate mais, no proximo elevador.
+                if encheu:
+                    break
+        if len(self.noAndarParaSair) == 0:
+            if elev.direcao == cEstadoElevador.SUBINDO:
+                self.subir.desativa()
+            elif elev.direcao == cEstadoElevador.DESCENDO:
+                self.descer.desativa()
+            
+        
+    def saiDoElevador(self, pessoas ):    
+        # Tira todas as pessoas desse elevador,
+        for P in pessoas:
+            #
+            self._lock.acquire()
+            self.noAndarOcupado[ P.chave ] = P
+            P.ocupada(self)
+            self._lock.release()
+            #
+            
+    def estouSaindo(self,P):
+        #
+        self._lock.acquire()
+        self.noAndarOcupado.pop( [P.chave] )
+        self.noAndarParaSair[P.chave] = P
+        self._lock.release()
 
+        
 
 class cPredio( cDestino ):
  
-    def __init__( self, destino, andares ):
+    def __init__( self, destino, andares, nElevadores = 5 ):
         super().__init__( destino )
         # inicializa o predio com seus andares,
         #
         self.niveis = cAndar( andares )
-        self.elevadores =  list( cElevador( N , andares ) for N in range(5) )
+        self.elevadores =  list( cElevador( N , andares ) for N in range(nElevadores) )
 
 
-        print("Elevadores >> \n")
+        logging.info( "{} Elevadores >> \n".format( super().__str__() ) )
         for C in self.elevadores:
-            print( "{} , {}\n".format( C,  str(C) ) )
+            logging.info( C )
+        
+
+class cBairro():
+
+    def __init__(self):
+        (self.cdBairro, self.nomeBairro) = Nomes.Bairro()
+        self.Predios = []
+        
+
+    def novoPredio(self, destino, andares ):
+        destino.cdBairro = self.cdBairro
+        destino.nivel = andares
+        elev = (andares % 4) + 2
+        self.Predios.append( cPredio( destino, andares,  nElevadores = elev)
+        
+
+    def incluiPredio(self, predio):
+        self.Predios.append(predio)
         
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
     pessoasX = []
-    predios = []
+    bairro = cBairro()
 
     
     for px in range(1,10):
-        predios.append( cPredio( cDestino("Predio {}".format(px) ) , int(random.random() * 10) ) )
+        bairro.incluiPredio( cPredio( cDestino("Predio {}".format(px) ) , int(random.random() * 10) ) )
 
-    for P in nomes.nomesPessoas():
+    CP = Cadastros.Pessoa()
+    for px in range( 1, 20 ):
+        P = next(CP) 
         s =  cPessoa(P, cDestino("casa") , cDestino("casa") )
-        # print( s )
         pessoasX.append( cPessoa(P, cDestino("casa") , cDestino("casa") )  )
 
-    print( len(pessoasX) , [ str(P) for P in pessoasX ] )
+    logging.info( "Pessoas: {} :: {}".format( len(pessoasX) , [ str(P) for P in pessoasX ] )
 
     
