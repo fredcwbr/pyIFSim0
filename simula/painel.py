d476645cb239b9a002118c5f2739b8a696d9dd34
Frederico C Wilhelms <fredcw@gmail.com>
2024-12-31 13:29:00 -0300

    Painel com funcao de teste funcional Multiplas threads estao operando normalmente,
    Ajustar o exercitador para tratar a atualizacao de uma coluna
    de forma automatica.

--- simula/painel.py
+++ simula/painel.py
@@ -4,12 +4,10 @@ import os
 import sys
 import time
 import queue
+from queue import Empty as qEmpty
 import threading
 from threading import Thread
 
-
-
-window = tk.Tk()
 
 
 class tk_pessoa():
@@ -18,21 +16,6 @@ class tk_pessoa():
 class tk_andar():
     pass
 
-
-class traverse():
-
-    def __init__(self):
-        self.coluna = []
-
-    def insere(self, btn ):
-        self.coluna.append( btn )
-
-    def trvrse(self):
-        for I in range(len(self.coluna)):
-            yield (self.coluna[I],'U')
-        for I in reversed(range(len(self.coluna))):
-            yield (self.coluna[I],'D')
-        
 
 class tk_btx(tk.Button):
     
@@ -52,18 +35,16 @@ class tk_predio(tk.Frame):
     def __init__(self,parent, elevadores,andares,*args,**kwargs):
         super().__init__(parent)
         #
-        self.tvs = []
-        self.slot =  [ [] for A in range( andares +1 )  ]
+        self.slot =  [  [ [] for B in range(andares) ] for A in range(elevadores) ]
         self.elevadores = elevadores
         #
-        l_final = andares + 3
+        l_inicial = 3
         l0 = tk.Label( parent ,
                        text = kwargs.get("NomePredio", "Predio :: XYZ" )
                     )
         l0.grid( row=0, column=0, columnspan= elevadores )
                        
         for E in range(elevadores):
-            self.tvs.append(  traverse() )
             for A in range(andares):
                 L = tk.Label( parent,
                               text = "Andar {}".format(A) if A > 0 else "Saguao"
@@ -78,62 +59,77 @@ class tk_predio(tk.Frame):
                            andar = A,
                            elevador = E
                     )
-                self.tvs[E].insere(X)
-                L.grid(row=l_final-A,column=0,pady=3)
-                X.grid(row=l_final-A,column=E+1,pady=2)
-                self.slot[andares-A].append( X )
+                L.grid(row=l_inicial+andares-A,column=0,pady=3)
+                X.grid(row=l_inicial+andares-A,column=E+1,pady=2)
+                self.slot[E][A].append( X )
+        print( self.slot )
 
-    
+        
     def exercitaElevador(self,mainWin, fila):
         self.fila = fila
         self.mainWin = mainWin
-        self.exercita()
-
-    def exercita(self):
-        # Executa a movimentacao
         #
         while( True ):
-            for E in range(self.elevadores):
-                for I,M in self.tvs[E].trvrse():
-                    # Cria uma thread para esse exercicio
-                    self.fila.put( (I, "Mx"+M ) )
-                    self.mainWin.event_generate('<<atuElevador>>')
-                    time.sleep(1)
-                # Espera ate a proxima interacao ,
-                # << Atualiza o estado do painel monitor.
+            # Para cada Elevador,
+            for N in range( len(self.slot) ):
+                Mx = random.randrange( len(self.slot[ N ]) )
+                # Indicadores do painel
+                for MN in range(len(self.slot[ N ])):
+                    for E in self.slot[ N ][MN]:
+                        # Atualiza o elevador neste momento., 
+                        # Cria uma thread para esse exercicio
+                        MSG =  " " if MN != Mx else "Mx"
+                        self.fila.put( ( E , MSG ) )
+                print( "Enviados: {} msgs".format(self.fila.qsize() ) )
+                self.mainWin.event_generate('<<atuElevador>>')
+                time.sleep(1)
+            # Espera ate a proxima interacao ,
+            # << Atualiza o estado do painel monitor.
             time.sleep(1)
             
 def processaElevador(mqueue , evento):
     # ** Levar em consideracao que
     # pode nao ter nada na fila.( ainda )
     #
-    x,msg = mqueue.get_nowait()
-    print("ProcessaElevador executado")
-    x.configure(text=msg)
+    # print("ProcessaElevador executado")
+    try:
+        while True:
+            x,msg = mqueue.get(block=False)
+            print( x, msg ) 
+            x.configure(text=msg)
+    except qEmpty:
+        pass
     # atualizar a tela.,
-    pass
-            
+    
+
+
+class interfacePredio:
+    def __init__( self, mqElevador, elevadores, andares ):
+        self.window = tk.Tk()
+        pnl1 = tk.Frame(self.window)
+        pnl1.grid(row=1,column=2,padx=2,pady=2)
+        P = tk_predio(pnl1, elevadores ,andares)
+        P.grid(row=1,column=3,pady=2,padx=2)
+        # VEvent --> 
+        self.window.bind('<<atuElevador>>', lambda e: processaElevador(mqElevador,e) )
+        t = threading.Thread(target=P.exercitaElevador, args=(self.window, mqElevador,),daemon=True)
+        t.start()
+        self.window.mainloop()
 
 if __name__ == '__main__':
 
-    mqElevador = queue.Queue()
-    
-    elev = 3
+    elev = 1
     andares = 5
-    
-    
-    pnl1 = tk.Frame(window)
-    pnl1.grid(row=1,column=2,padx=2,pady=2)
-    
-    P = tk_predio(pnl1, elev,andares)
-    P.grid(row=1,column=3,pady=2,padx=2)
 
-    # VEvent --> 
-    window.bind('<<atuElevador>>', lambda e: processaElevador(mqElevador,e) )
+    for I in [ 1 ]:
+        mqElevador = queue.Queue()
+        t = threading.Thread(
+            target=interfacePredio,
+            args=(mqElevador, elev+I , andares + I ),
+            daemon=True
+        )
+        t.start()
 
-    t = threading.Thread(target=P.exercitaElevador, args=(window, mqElevador,),daemon=True)
-    t.start()
         
-    window.mainloop()
-
+    
     
